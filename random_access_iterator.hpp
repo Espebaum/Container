4598f9ee884420b78c24d78924b1c0389eb64cc8
Espebaum/gyopark <vocops5000@gmail.com>
2024-05-14 20:42:37 +0900

    container

--- random_access_iterator.hpp
+++ random_access_iterator.hpp
@@ -5,7 +5,25 @@
 namespace ft
 {
     template<typename Iterator>
-    class random_access_iterator {
+    class random_access_iterator 
+    {
+
+        private:
+            Iterator    _iter; // 실제 반복자를 저장할 멤버 변수
+
+        public:
+            // 반복자 타입 정의
+            typedef typename iterator_traits<Iterator>::iterator_category   iterator_category;
+            typedef typename iterator_traits<Iterator>::value_type          value_type;
+            typedef typename iterator_traits<Iterator>::difference_type     difference_type;
+            typedef typename iterator_traits<Iterator>::pointer             pointer;
+            typedef typename iterator_traits<Iterator>::reference           reference;    
+
+            // 생성자와 소멸자
+            random_access_iterator() : _iter(NULL) {}
+            random_access_iterator(Iterator iter) : _iter(iter) {}
+            random_access_iterator(const random_access_iterator &ref) : _iter(ref._iter) {}
+            template
 
     };
 }
