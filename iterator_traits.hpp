dc70b214cfccdf1838f8a98b02b03c3677b3913b
Espebaum/gyopark <vocops5000@gmail.com>
2024-05-14 22:41:17 +0900

    container

--- iterator_traits.hpp
+++ iterator_traits.hpp
@@ -6,7 +6,8 @@
 namespace ft
 {
     // #include <iterator> std::random_access_iterator_tag
-    // 위 사용이 제한되어있기 때문에 아래 tag struct를 사용함
+    // 위 사용이 제한되어있기 때문에 아래 tag struct를 사용함 
+    // libstdc++의 stl_iterator_base_types.h 참조
     struct input_iterator_tag {};
     struct output_iterator_tag {};
     struct forward_iterator_tag : public input_iterator_tag {};
@@ -21,8 +22,10 @@ namespace ft
         typedef Distance    difference_type;
         typedef Pointer     pointer;
         typedef Reference   reference;
-    };  // 내부적인 구조, 사용자가 정의하는 iterator는 위의 요소들을 포함하고 있다.
-        // 이 과정에서 iterator_traits가 이용된다.
+    };  // 내부적인 구조, 사용자가 정의하는 iterator는 위의 요소들을 포함하고 있다(포함해야 한다).
+        // 그러니까, 이 구조체 자체가 코드에서 무언가를 하는 것은 아니다.
+        // 즉 인터페이스의 역할을 한다고 볼 수 있다.
+        // iterator_traits는 iterator 구조체를 통해 반복자 타입의 논리적 정보를 추출한다.
 
     // iterator_traits의 구현부
     // 일반 iterator의 경우 아래 템플릿을 통해 생성
@@ -35,6 +38,7 @@ namespace ft
         typedef typename Iterator::pointer              pointer;
         typedef typename Iterator::reference            reference; 
     };
+    
     // 포인터 형태로 iterator가 사용될 경우 아래 템플릿 활용
     template<typename T>
     struct iterator_traits<T*>
--- random_access_iterator.hpp
+++ random_access_iterator.hpp
@@ -7,8 +7,7 @@ namespace ft
     template<typename Iterator>
     class random_access_iterator 
     {
-
-        private:
+        protected:
             Iterator    _iter; // 실제 반복자를 저장할 멤버 변수
 
         public:
@@ -20,10 +19,90 @@ namespace ft
             typedef typename iterator_traits<Iterator>::reference           reference;    
 
             // 생성자와 소멸자
-            random_access_iterator() : _iter(NULL) {}
             random_access_iterator(Iterator iter) : _iter(iter) {}
-            random_access_iterator(const random_access_iterator &ref) : _iter(ref._iter) {}
-            template
+           
+            explicit
+            random_access_iterator(const  &ref) : _iter(ref._iter) {}
+            
+            // Iterator로 const Iterator를 초기화할 수 있도록 한다. (gnu reference)
+            template<typename Iter>
+            random_access_iterator(const random_access_iterator<Iter> &ref) : _iter(ref.base()) {}
+
+            // 현재 위치인 기점 반복자(base iterator) 반환, 모든 유형의 반복자에서 사용
+            const Iterator& 
+            base() { return _iter; }
+            
+            // --- 순방향 반복자 필요 기능 --- //
+            reference
+            operator*() const { return *_iter; }
+            
+            pointer
+            operator->() const { return _iter; }
+            
+            // 전위 증가 연산
+            random_access_iterator&
+            operator++() 
+            { 
+                ++_iter; 
+                return *this;
+            }
+
+            // 후위 증가 연산
+            random_access_iterator 
+            operator++(int) { return (random_access_iterator(_iter++)); }
+
+            // --- 양방향 반복자 필요 기능 --- //
+            // 전위 감소 연산
+            random_access_iterator&
+            operator--()
+            {
+                --_iter;
+                return *this;
+            }
+    
+            // 후위 감소 연산
+            random_access_iterator
+            operator--(int) { return (random_access_iterator(_iter--)); }
+    
+            // --- 임의 접근 반복자 필요 기능 --- //
+            reference
+            operator[] (difference_type _n) const
+            { return _iter[_n]; }
+
+            random_access_iterator&
+            operator+=(difference_type _n)
+            {
+                _iter += _n;
+                return *this;
+            }
+
+            random_access_iterator
+            operator+(difference_type _n) 
+            const { return (random_access_iterator(_iter + _n)); }
+
+            random_access_iterator&
+            operator-=(difference_type _n)
+            {
+                _iter -= _n;
+                return *this;
+            }
+
+            random_access_iterator
+            operator-(difference_type _n)
+            const { return (random_access_iterator(_iter - _n)); }
+
+            // -- 순방향 반복자 필요 기능 --//
+            template<typename _IteratorL, typename _IteratorR>
+            inline bool
+            operator==(const random_access_iterator<IteratorL>& _lhs,
+                    const random_access_iterator<IteratorR>& _rhs)
+            { return _lhs.base() == _rhs.base(); }
+
+            template<typename _IteratorL, typename _IteratorR>
+            inline bool
+            operator!=(const random_access_iterator<IteratorL>& _lhs,
+                    const random_access_iterator<IteratorR>& _rhs)
+            { return _lhs.base() != _rhs.base(); }
 
     };
 }
